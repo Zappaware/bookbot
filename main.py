2cc60ef373c1c3236d48bbc170274117f5e77366
Zappaware <sergio_germanc@hotmail.com>
2025-07-15 12:30:41 -0600

    a description of your changes here

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sys
 from stats import count_words, count_characters, sort_dict
 
-path = "/home/sergi/bookbot/books/frankenstein.txt"
+
 
 def get_book_text(path):
     with open(path) as f:
@@ -8,14 +9,19 @@ def get_book_text(path):
         return file_contents
 
 def main():
-    word_count_in_text = count_words(get_book_text(path))
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...") 
-    print("----------- Word Count ----------")
-    print(f"Found {word_count_in_text} total words")
-    print("--------- Character Count -------")
-    char_count_in_text = sort_dict(count_characters(get_book_text(path)))
-    print("============= END ===============""")
+    num_arg = len(sys.argv)
+    if num_arg == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        word_count_in_text = count_words(get_book_text(sys.argv[1]))
+        print("============ BOOKBOT ============")
+        print("Analyzing book found at books/frankenstein.txt...") 
+        print("----------- Word Count ----------")
+        print(f"Found {word_count_in_text} total words")
+        print("--------- Character Count -------")
+        char_count_in_text = sort_dict(count_characters(get_book_text(sys.argv[1])))
+        print("============= END ===============""")
 
 main()
 
